e98b6c79cf0b609ab9596aecd67c233728199abd
Jakub Červenka <kuba@gymspit.cz>
2020-09-23 09:40:45 +0200

    Add lecture 4

---
+++ lecture-4/MyLinkedList.cs
@@ -0,0 +1,82 @@
+﻿using System;
+// https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.linkedlist-1?view=netcore-3.1
+
+
+namespace Lecture4
+{
+	class MyLinkedList<T>
+	{
+		public class MyListNode {
+			public T Item { get; private set; }
+			public MyListNode next = null;
+
+
+			public MyListNode(T item)
+			{
+				Item = item;
+			}
+		}
+
+		private MyListNode first = null;
+
+		private MyListNode last = null;
+
+		private int count = 0;
+
+
+		public MyListNode GetFirst()
+		{
+			return first;
+		}
+
+
+		public MyListNode GetLast()
+		{
+			return last;
+		}
+
+
+		public void AddFirst(T item)
+		{
+			MyListNode node = new MyListNode(item);
+			node.next = first;
+			first = node;
+			if (last == null) {
+				last = node;
+			}
+			count += 1;
+		}
+
+
+		public void AddLast(T item)
+		{
+			MyListNode node = new MyListNode(item);
+			if (first == null) {
+				first = node;
+			}
+			if (last != null) {
+				last.next = node;
+			}
+			last = node;
+			count += 1;
+		}
+
+
+		public void AddAfter(T item, MyListNode after)
+		{
+			MyListNode node = new MyListNode(item);
+			node.next = after.next;
+			after.next = node;
+			if (last == after) {
+				last = node;
+			}
+			count += 1;
+		}
+
+
+		public int Count()
+		{
+			return count;
+		}
+	}
+}
---
+++ lecture-4/MyList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+// https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=netcore-3.1
+
+
+namespace Lecture4
+{
+	class MyList<T>
+	{
+		private const int INITIAL_CAPACITY = 10;
+
+		private T[] items = new T[INITIAL_CAPACITY];
+
+		private int count = 0;
+
+		private int capacity = INITIAL_CAPACITY;
+
+
+		public T Get(int i)
+		{
+			if (i < 0 || i >= count) {
+				throw new ArgumentOutOfRangeException();
+			}
+
+			return items[i];
+		}
+
+
+		public void Add(T item)
+		{
+			if (count >= capacity) {
+				capacity *= 2;
+				Array.Resize(ref items, capacity);
+			}
+
+			items[count] = item;
+			count += 1;
+		}
+
+
+		public int Count()
+		{
+			return count;
+		}
+	}
+}
